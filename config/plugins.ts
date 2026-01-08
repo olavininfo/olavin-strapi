f40ddfa0d5c157b7b26589fada2f2b40366c91cd
Olavininfo <olavininfo@gmail.com>
2026-01-08 15:30:40 +0800

    fix: install and configure R2 provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,23 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        s3Options: {
+          accessKeyId: env('S3_ACCESS_KEY_ID'),
+          secretAccessKey: env('S3_ACCESS_SECRET'),
+          endpoint: env('S3_ENDPOINT'),
+          region: env('S3_REGION'),
+          params: {
+            Bucket: env('S3_BUCKET'),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
